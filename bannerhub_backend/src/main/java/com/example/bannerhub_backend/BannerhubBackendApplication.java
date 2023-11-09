4736bd624a9b40f39feab103d981df227a3daf0a
Denis Chernov <chernoff.cs@gmail.com>
2023-11-09 18:20:40 +0200

    Initializing backend part

---
+++ bannerhub_backend/src/main/java/com/example/bannerhub_backend/BannerhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bannerhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerhubBackendApplication.class, args);
+	}
+
+}
